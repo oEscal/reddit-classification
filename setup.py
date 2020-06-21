b215d70c7a4a3e241ec24e77c8881d366690d0f5
escaleira <pedroescaleira@hotmail.com>
2020-06-21 02:29:18 +0100

    ...

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-<<<<<<< HEAD
     name='reddit-classification',
-    version='0.0.1',
-=======
-    name='reddit_classification',
     version='0.0.2',
->>>>>>> d1d72c5d8d5513351096b2bf040f1e2d891b3b8a
     description='TAA project 2 package',
     author='Pedro Escaleira and Rafael Simões',
     author_email='pedroescaleira@hotmail.com',
