6a3e754a4fff2a304eed1d71fb46e4a9b62f75eb
escaleira <pedroescaleira@hotmail.com>
2020-06-23 00:03:42 +0100

    more work done

--- reddit_bayes/main.py
+++ reddit_bayes/main.py
@@ -5,8 +5,8 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.naive_bayes import MultinomialNB
 
-
-from reddit_classification.utils import convert_input
+from reddit_classification.graphics import data_distribution
+from reddit_classification.utils import convert_input, occurrences_per_label
 from reddit_classification.data_treatment import read_data
 
 
@@ -15,54 +15,77 @@ def main():
 	from https://www.kaggle.com/mswarbrickjones/starter-naive-bayes-benchmark-p-at-5-0-88
 	"""
 
-	initial_time = time.time()
-	number_classes = 0
+	number_classes = 20
 	if number_classes:
 		X, y = read_data(f"data/pruned_{number_classes}_entries.tsv")
 	else:
 		X, y = read_data()
 
-	train_split_index = int(len(X) * 0.8)
+	train_split_index = int(len(X) * 0.6)
+	cv_split_index = train_split_index + int(len(X) * 0.2)
+	# 20% for cross validation and 20% for test
 
-	X_train, X_test = X[:train_split_index], X[train_split_index:]
-	y_train, y_test = y[:train_split_index], y[train_split_index:]
+	X_train, X_cv = X[:train_split_index], X[train_split_index:cv_split_index]
+	y_train, y_cv, y_test = y[:train_split_index], y[train_split_index:cv_split_index], y[cv_split_index:]
+
+	occurrences_per_label_dict = occurrences_per_label(y_train)
+	print(len(occurrences_per_label_dict))
+	data_distribution("Train data distribution", list(occurrences_per_label_dict.keys()),
+	                  list(occurrences_per_label_dict.values()), f"reddit_bayes/graphs/train_data_distribution.png")
+	occurrences_per_label_dict = occurrences_per_label(y_cv)
+	print(len(occurrences_per_label_dict))
+	data_distribution("Cross Validation data distribution", list(occurrences_per_label_dict.keys()),
+	                  list(occurrences_per_label_dict.values()), f"reddit_bayes/graphs/cv_data_distribution.png")
+	occurrences_per_label_dict = occurrences_per_label(y_test)
+	print(len(occurrences_per_label_dict))
+	data_distribution("Test data distribution", list(occurrences_per_label_dict.keys()),
+	                  list(occurrences_per_label_dict.values()), f"reddit_bayes/graphs/test_data_distribution.png")
 
 	label_encoder = LabelEncoder()
 	label_encoder.fit(y_train)
 	y_train = label_encoder.transform(y_train)
-	y_test = label_encoder.transform(y_test)
+	y_cv = label_encoder.transform(y_cv)
 
 	# extract features
-	X_train, X_test, token_frequencies = convert_input(X_train, X_test)
+	X_train, X_cv, token_frequencies = convert_input(X_train, X_cv, limit=1000, min_df=1)
 
 	# save token frequencies
 	with open(f"reddit_bayes/token_frequencies/token_frequencies_{number_classes}", 'wb') as file:
 		pickle.dump(token_frequencies, file)
 
+	# with open(f"reddit_bayes/token_frequencies/token_frequencies_{number_classes}", 'rb') as file:
+	# 	token_frequencies = pickle.load(file)
+	# 	X_train = token_frequencies.fit_transform(X_train).todense()
+	# 	X_cv = token_frequencies.transform(X_cv).todense()
+
+	# normalize
+	X_train = X_train / np.max(X_train)
+	X_cv = X_cv / np.max(X_cv)
+
 	# train
-	model = MultinomialNB(alpha=0.1)
-	model.fit(X_train, y_train)
+	initial_alpha = 1e-10
+	while initial_alpha <= 1:
+		for i in [1, 5]:
+			alpha = initial_alpha * i
 
-	# save model
-	with open(f"reddit_bayes/models/model_{number_classes}", 'wb') as file:
-		pickle.dump(model, file)
+			initial_time = time.time()
+			model = MultinomialNB(alpha=alpha)
+			model.fit(X_train, y_train)
 
-	y_pred_proba = model.predict_proba(X_test)
-	y_pred = np.argmax(y_pred_proba, axis=1)
+			# save model
+			with open(f"reddit_bayes/models/model_{number_classes}_{format(alpha, '.10g')}", 'wb') as file:
+				pickle.dump(model, file)
 
-	def precision_at_k(y_true, y_pred, k=5):
-		y_true = np.array(y_true)
-		y_pred = np.array(y_pred)
-		y_pred = np.argsort(y_pred, axis=1)
-		y_pred = y_pred[:, ::-1][:, :k]
-		arr = [y in s for y, s in zip(y_true, y_pred)]
-		return np.mean(arr)
+			# with open(f"reddit_bayes/models/model_{number_classes}_{alpha}", 'rb') as file:
+			# 	model = pickle.load(file)
 
-	print('precision@1 =', np.mean(y_test == y_pred))
-	print('precision@3 =', precision_at_k(y_test, y_pred_proba, 3))
-	print('precision@5 =', precision_at_k(y_test, y_pred_proba, 20))
+			y_pred_proba = model.predict_proba(X_cv)
+			y_pred = np.argmax(y_pred_proba, axis=1)
 
-	print(time.time() - initial_time)
+			print(f"precision for alpha {format(alpha, '.10g')} = {np.mean(y_cv == y_pred)}")
+
+			print(time.time() - initial_time)
+		initial_alpha *= 10
 
 
 if __name__ == "__main__":
--- reddit_logistic/main.py
+++ reddit_logistic/main.py
@@ -1,10 +1,87 @@
+import pickle
+import time
+
+import numpy as np
+from sklearn.ensemble import VotingClassifier
+import matplotlib as mp
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.naive_bayes import MultinomialNB
+
+
+from reddit_classification.utils import convert_input
+from reddit_classification.data_treatment import read_data
+
+
+def evaluate_model(model, X, y, X_test, y_test, target_names=None):
+	scores = cross_val_score(model, X, y, cv=5, scoring='accuracy')
+	scores_test = cross_val_score(model, X_test, y_test, cv=5, scoring='accuracy')
+
+	print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std()))
+	print("Accuracy test: %0.2f (+/- %0.2f)" % (scores_test.mean(), scores_test.std()))
+
+	print("Test classification report: ")
+	if target_names is None:
+		target_names = model.classes_
+	# print(classification_report(y_test, model.predict(X_test), target_names=target_names))
+	print("Test confusion matrix: ")
+	# print_confusion_matrix(confusion_matrix(y_test, model.predict(X_test)), class_names=target_names)
 
 
 def main():
 	"""
 	from https://www.kaggle.com/balatmak/text-classification-pipeline-newsgroups20
 	"""
-	pass
+
+	number_classes = 50
+	if number_classes:
+		X, y = read_data(f"data/pruned_{number_classes}_entries.tsv")
+	else:
+		X, y = read_data()
+
+	train_split_index = int(len(X) * 0.6)
+	cv_split_index = train_split_index + int(len(X) * 0.2)
+	# 20% for cross validation and 20% for test
+
+	X_train, X_cv = X[:train_split_index], X[train_split_index:cv_split_index]
+	y_train, y_cv = y[:train_split_index], y[train_split_index:cv_split_index]
+
+	label_encoder = LabelEncoder()
+	label_encoder.fit(y_train)
+	y_train = label_encoder.transform(y_train)
+	y_cv = label_encoder.transform(y_cv)
+
+	# extract features
+	# X_train, X_test, token_frequencies = convert_input(X_train, X_test, limit=1000, min_df=5)
+
+	# save token frequencies
+	# with open(f"reddit_bayes/token_frequencies/token_frequencies_{number_classes}", 'wb') as file:
+	# 	pickle.dump(token_frequencies, file)
+
+	with open(f"reddit_bayes/token_frequencies/token_frequencies_{number_classes}", 'rb') as file:
+		token_frequencies = pickle.load(file)
+		X_train = token_frequencies.fit_transform(X_train).todense()
+		X_cv = token_frequencies.transform(X_cv).todense()
+
+	# normalize
+	X_train = X_train / np.max(X_train)
+	X_cv = X_cv / np.max(X_cv)
+
+	c_s = [1, 10, 100, 1000, 10000]
+	for c in c_s:
+		initial_time = time.time()
+		model = LogisticRegression(C=c, multi_class='ovr', max_iter=1000)
+
+		model.fit(X_train, y_train)
+
+		with open(f"reddit_logistic/models/model_{number_classes}_{c}", 'wb') as file:
+			pickle.dump(model, file)
+
+		evaluate_model(model, X_train, y_train, X_cv, y_cv)
+
+		print(f"\ntime: {time.time() - initial_time}\n\n\n")
 
 
 if __name__ == "__main__":
